d24704437a0376034c857aaaeaead853617ad8e0
Andy Lo-A-Foe <andy.loafoe@gmail.com>
2021-11-30 21:43:27 +0100

    Stubbed version that mimics containerd client

--- client.go
+++ client.go
@@ -1,6 +1,9 @@
 package containerh
 
 import (
+	"context"
+
+	"github.com/containerd/containerd"
 	"github.com/philips-software/go-hsdp-api/cartel"
 )
 
@@ -10,4 +13,36 @@ type Client struct {
 
 	Instances *InstancesService
 	CLI       *CLIServices
+}
+
+// Version of containerd
+type Version struct {
+	// Version number
+	Version string
+	// Revision from git that was built
+	Revision string
+}
+
+func (c *Client) IsServing(ctx context.Context) (bool, error) {
+	// TODO: implement properly
+	return true, nil
+}
+
+func (c *Client) Version(ctx context.Context) (Version, error) {
+	return Version{
+		Version:  "0.0.1",
+		Revision: "deadbeef",
+	}, nil
+}
+
+func (c *Client) Pull(ctx context.Context, imageName string, opts ...containerd.RemoteOpt) (containerd.Image, error) {
+	return nil, nil
+}
+
+func (c *Client) NewContainer(ctx context.Context, containerName string, runtime containerd.NewContainerOpts, snapshot containerd.NewContainerOpts, spec containerd.NewContainerOpts) (containerd.Container, error) {
+	return nil, nil
+}
+
+func (c *Client) LoadContainer(ctx context.Context, id string) (containerd.Container, error) {
+	return nil, nil
 }
